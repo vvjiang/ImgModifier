62afb541c3deedd715eb621082199e403ea555fe
韩子卢 <vvjiang@foxmail.com>
2018-03-22 19:39:16 +0800

    去掉设置大小

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import './app.css';
 
 /**
  * 设置canvas图像到下载链接上
@@ -7,7 +8,9 @@ const setCanvasImgToDownloadLink = () => {
   const imgData = document.getElementById('target_canvas').toDataURL();
   $('#download_file').attr('href', imgData);
 };
-
+const setCanvasSize = (width, height) => {
+  $('#target_canvas').attr('width', width).attr('height', height);
+};
 
 const loadImgFileInCanvas = (imgFile) => {
   const reader = new FileReader();
@@ -29,15 +32,10 @@ const loadImgFileInCanvas = (imgFile) => {
   reader.onloadend = (e) => {
     const dataURL = e.target.result;
     const img = new Image();
-    const ctx = document.getElementById('target_canvas').getContext('2d');
-    img.onload = () => {
+    img.onload = (event) => {
+      setCanvasSize(event.target.naturalWidth, event.target.naturalHeight);
+      const ctx = document.getElementById('target_canvas').getContext('2d');
       ctx.drawImage(img, 0, 0);
-      ctx.beginPath();
-      ctx.moveTo(30, 96);
-      ctx.lineTo(70, 66);
-      ctx.lineTo(103, 76);
-      ctx.lineTo(170, 15);
-      ctx.stroke();
       setCanvasImgToDownloadLink();
     };
     img.src = dataURL;
@@ -45,14 +43,31 @@ const loadImgFileInCanvas = (imgFile) => {
   reader.readAsDataURL(imgFile);
 };
 
+
 // 绑定事件
 const bindEvent = () => {
   $('#target_file').change((event) => {
     const imgFile = event.target.files[0];
     loadImgFileInCanvas(imgFile);
   });
-  $('#btn_select_file').click((event) => {
+  $('#btn_select_file').click(() => {
     $('#target_file').click();
+  });
+  $('#set_text').click(() => {
+    const x = $('#position_x').val();
+    const y = $('#position_y').val();
+    const text = $('#inject_text').val();
+    const ctx = document.getElementById('target_canvas').getContext('2d');
+    const fontSize = Number.parseInt($('#text_size').val(), 10);
+    ctx.font = `${fontSize}px cursive`;
+    ctx.fillStyle = $('#text_color').val();
+    ctx.fillText(text, Number.parseInt(x, 10), Number.parseInt(y, 10) + fontSize);
+    setCanvasImgToDownloadLink();
+  });
+  $('#set_canvas').click(() => {
+    const width = $('#canvas_width').val();
+    const height = $('#canvas_height').val();
+    setCanvasSize(width, height);
   });
 };
 $(() => {
